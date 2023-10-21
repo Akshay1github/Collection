87f3d96e4c58f9a9afea9a3c6f19fb237554569d
Akshay1github <asontakke21@gmail.com>
2023-10-21 10:41:24 +0530

    Updated Collections Programs

--- src/com/comp/client/list/ListIterator.java
+++ src/com/comp/client/list/ListIterator.java
@@ -1,9 +1,43 @@
 package com.comp.client.list;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ListIterator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<String> list = new LinkedList<String>();
+		
+		list.add("Akshay");
+		list.add("Ronie");
+		list.add("Toshi");
+		list.add("Anna");
+		list.add("meow");
+		list.add("haha");
+		
+		java.util.ListIterator<String> itr = list.listIterator();
+		
+		while(itr.hasNext()) {
+			
+			String str = itr.next();
+			
+			if(str.equals("haha")) {
+				
+				//itr.remove();
+				itr.set("Newly added String");
+				
+			}
+			
+			List<Object> emptyList = Collections.emptyList();
+			System.out.println(emptyList);
+			
+		} for(String o:list) {
+			System.out.println(o);
+		}
+		
 
 	}
 
